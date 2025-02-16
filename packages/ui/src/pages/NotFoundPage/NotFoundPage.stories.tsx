8101e1638e32c992d1459f69c4152d5a06eceb7e
Huss Martinez <husse.dev@gmail.com>
2025-02-17 00:28:51 +0700

    feat: implemented NotFoundPage

---
+++ packages/ui/src/pages/NotFoundPage/NotFoundPage.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { NotFoundPage } from "./NotFoundPage";
+
+const CUSTOM_DESCRIPTION =
+  "Oops! We couldn’t find this voting page. Double-check the round ID/chain ID or reach out to your round admin for the correct link.";
+
+const meta: Meta<typeof NotFoundPage> = {
+  title: "Pages/NotFoundPage",
+  component: NotFoundPage,
+  argTypes: {
+    description: {
+      control: "text",
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+    },
+  },
+} satisfies Meta<typeof NotFoundPage>;
+
+export default meta;
+
+type Story = StoryObj<typeof NotFoundPage>;
+
+export const Default: Story = {};
+
+export const WithCustomDescription: Story = {
+  args: {
+    description: CUSTOM_DESCRIPTION,
+  },
+};
+
+export const WithButton: Story = {
+  args: {
+    button: {
+      label: "Go to home",
+      onClick: () => {
+        console.log("clicked");
+      },
+    },
+  },
+};
