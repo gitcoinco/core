e6bf9161c6151bf4bd4513f7701dc34b1ceaf53b
Huss Martinez <husse.dev@gmail.com>
2025-02-21 04:23:31 +0700

    style(ssr-app): changed padding and max-h

--- apps/next14-ssr-testing/app/primitives/[component]/page.tsx
+++ apps/next14-ssr-testing/app/primitives/[component]/page.tsx
@@ -13,7 +13,7 @@ export default function Page({ params }: { params: { component: string } }) {
       {cases.map(({ label, props }) => (
         <div key={label} className="flex flex-col items-center gap-2">
           <h2 className="text-lg font-bold">{label}</h2>
-          <div className="flex max-h-[70vh] flex-col items-center gap-2 overflow-y-auto rounded-lg border border-gray-200 p-6">
+          <div className="flex max-h-[80vh] flex-col items-center gap-2 overflow-y-auto rounded-lg border border-gray-200 p-6">
             {props.map(([key, props]) => (
               <Component key={key} {...props} />
             ))}
--- apps/next14-ssr-testing/app/primitives/layout.tsx
+++ apps/next14-ssr-testing/app/primitives/layout.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex gap-20 p-6">
+    <div className="flex gap-20 px-6">
       <div className="pt-8">
-        <div className="flex max-h-[70vh] flex-col gap-3 overflow-y-auto rounded-lg border border-gray-200 p-6">
+        <div className="flex max-h-[80vh] flex-col gap-3 overflow-y-auto rounded-lg border border-gray-200 p-6">
           <Link className="text-grey-500 font-bold" href="/">
             {"<- Back"}
           </Link>
