5fedf11ca86b9c2f92f853ddb162bb7e1a4a6b97
nijoe1 <nikolaos@gitcoin.co>
2025-03-03 17:53:09 -0800

    feat: added mock date decorator into storybook

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import { handlers } from "@repo/mocks/handlers";
 import { withThemeByDataAttribute } from "@storybook/addon-themes";
+import { mockDateDecorator } from "storybook-mock-date-decorator";
+
 import type { Preview } from "@storybook/react";
 import { initialize, mswLoader } from "msw-storybook-addon";
 
@@ -48,6 +50,7 @@ const preview: Preview = {
       defaultTheme: "light",
       attributeName: "data-theme",
     }),
+    mockDateDecorator,
   ],
 
   tags: ["autodocs"],
