f8a553c4e4b5c7e932937da9fff2d94b7ac55b28
nijoe1 <nikolaos@gitcoin.co>
2025-03-20 17:45:52 +0200

    chore: export Leaderboard

--- packages/ui/src/features/retrofunding/index.ts
+++ packages/ui/src/features/retrofunding/index.ts
@@ -3,3 +3,4 @@ export * from "./components/Landing";
 export * from "./components/BallotForm";
 export * from "./components/ProgramPickerModal";
 export * from "./components/Distribute";
+export * from "./components/Leaderboard";
